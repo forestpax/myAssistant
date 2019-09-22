4f2ff81ad617529ef16627343686c207361aae41
Hiroki OKAMOTO <hiroki.okamoto.forestpax@gmail.com>
2019-09-22 22:28:52 +0900

    options

---
+++ routes/get.js
@@ -0,0 +1,39 @@
+function refferRecord() {
+
+    var request = require('request');
+
+    var server = 'http://f9fe8d2c.ngrok.io'
+    //var server = 'http://localhost'
+
+    var siteId = 1
+
+
+
+    var url = server + "/pleasanter/api/items/" + siteId + "/get"
+
+    var options = {
+        uri: url,
+        method: 'POST',
+        encoding: 'utf8',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        json: {
+            'ApiKey': process.env.PLEASANTER_API_KEY,
+            'Offset': 0,
+            'View': {
+
+            }
+        }
+    };
+
+
+    request.post(options, function (error, response, body) {
+        for (var i = 0; i < body.Response.Data.length; i++){
+            console.log(body.Response.Data[i].Title);
+        }
+    });
+
+}
+
+refferRecord();
